c12567e2d66d19e6d6eac2556cad74b36c1180d9
jocen01 <jonathan.cederlund@gmail.com>
2022-09-18 13:31:27 +0200

    fixed parse of JSON from afbostäder api

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,7 +2,7 @@ import sttp.client3._
 import org.json4s._
 import org.json4s.native.JsonMethods._
 
-def getHouses() =
+def getHousesJson():String =
   val request = basicRequest.get(uri"https://api.afbostader.se:442/redimo/rest/vacantproducts?lang=sv_SE&type=1")
   val backend = HttpClientSyncBackend()
   val response = request.send(backend)
@@ -13,6 +13,14 @@ def getHouses() =
   }
 
 @main def hello: Unit = 
-  println(parse(getHouses()))
+  println(getHousesAtDelfi())
 
+def getHouses():Vector[Map[String,Any]] =
+  implicit val formats = org.json4s.DefaultFormats
+  val produktsAndError = parse(getHousesJson()).extract[Map[String,Vector[Map[String,Any]]]]
+  val product = produktsAndError("product")
+  product
+
+def getHousesAtDelfi() =
+  getHouses().filter(x => x("area") == "Delfi")
 def msg = "I was compiled by Scala 3. :)"
